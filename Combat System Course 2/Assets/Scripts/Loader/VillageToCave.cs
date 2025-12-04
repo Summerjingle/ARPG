4a154155564cc57a244ef9a42d5dcfc3e4e8eddb
Summerjingle <101238724+Summerjingle@users.noreply.github.com>
2025-12-04 17:09:25 +0800

    传送动画增加+游戏场景一些尝试
    1.更新了村庄To洞穴的切换场景动画，现在玩家不再是碰到trigger就自动传送，而是增加了交互选择，按下e后，播放一个特写传送动画，再进行场景切换
    2.使用松树代替了原来的榕树，并使用shader graph在村庄画面制作了一片云海

--- Combat System Course 2/Assets/Scripts/Loader/VillageToCave.cs
+++ Combat System Course 2/Assets/Scripts/Loader/VillageToCave.cs
@@ -1,3 +1,5 @@
+using Cinemachine;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,28 +7,88 @@ public class VillageToCave : MonoBehaviour
 {
     public string gameSceneName = "01Scene_Cave";
     public string loadingSceneName = "LoadingScene";
-    private bool hasTriggered=false;
+    public GameObject eTips;
+    public Animator playerAnim;
+    private bool isInTrigger=false;
+    private bool isTeleporting = false;
+    public CinemachineVirtualCamera TeleportCam;
+    public Animator teleportLight;
+
+
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player")&&!isInTrigger)
+        {
+            isInTrigger = true;
+            if(eTips!=null)
+                eTips.SetActive(true);
+            
+        }
+    }
+   private void OnTriggerExit(Collider other)
+    {
         if (other.CompareTag("Player"))
         {
+            isInTrigger=false;
+            if (eTips != null)
+                eTips.SetActive(false);
             
-            if (SaveManager.Instance != null)
-            {
-                if (hasTriggered) return;
-                hasTriggered = true;
-
-                SaveManager.Instance.SaveGame();
-                SaveManager.Instance.currentSaveData.currentScene = gameSceneName;
-                SaveManager.isNewGame = false;
-            }
-
-            // 设置目标场景并加载
-            PlayerPrefs.SetString("TargetScene", gameSceneName);
-            SaveManager.shouldLoadFromSave = true;
-            SaveManager.shouldLoadPosition = false; 
-            SceneManager.LoadScene(loadingSceneName);
         }
+    }
+    private void Update()
+    {
+        if (isInTrigger && Input.GetKeyDown(KeyCode.E) && !isTeleporting)
+        {
+            if (eTips != null)
+                eTips.SetActive(false);
+            Vector3 targetPosition = new Vector3(25.908f, 4.8f, -60.088f);  // 目标位置
+            Quaternion targetRotation = Quaternion.Euler(0, 128.346f, 0);  // 目标旋转
+            TeleportCam.Priority = 30;
+            playerAnim.gameObject.transform.position = targetPosition;
+            playerAnim.gameObject.transform.rotation = targetRotation;
+            StartTeleportSequence();
+        }
+    }
+
+    private void StartTeleportSequence()
+    {
+        playerAnim.gameObject.GetComponent<PlayerFighter>().InAction = true;
+        UIStateManager.SetUIActive(true);
+        if (playerAnim != null)
+            playerAnim.CrossFade("TeleportEnter", 0.1f);
+
+        StartCoroutine(TeleportRoutine());
+    }
+
+    private IEnumerator TeleportRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+        teleportLight.SetTrigger("StartTeleport");
+    //    if (isTeleporting) yield break;
+    //    isTeleporting = true;
+
+    //    // 等待动画播放（如果有的话）
+    //    if (playerAnim != null)
+    //        yield return new WaitForSeconds(5.0f); // 调整时间为动画长度
+
+    //    // 保存游戏数据
+    //    if (SaveManager.Instance != null)
+    //    {
+    //        if (hasTriggered) yield break;
+    //        hasTriggered = true;
+
+    //        SaveManager.Instance.SaveGame();
+    //        SaveManager.Instance.currentSaveData.currentScene = gameSceneName;
+    //        SaveManager.isNewGame = false;
+    //    }
+
+    //    // 设置目标场景并加载
+    //    PlayerPrefs.SetString("TargetScene", gameSceneName);
+    //    SaveManager.shouldLoadFromSave = true;
+    //    SaveManager.shouldLoadPosition = false;
+
+    //    // 加载场景
+    //    SceneManager.LoadScene(loadingSceneName);
     }
 }
